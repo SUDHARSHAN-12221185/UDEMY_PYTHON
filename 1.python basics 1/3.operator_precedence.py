5ddeef031685bff95d80492809c21ebbe3176d4a
Sudharshan Reddy <119558855+SUDHARSHAN-12221185@users.noreply.github.com>
2022-12-16 08:44:21 +0530

    Add files via upload

---
+++ 1.python basics 1/3.operator_precedence.py
@@ -0,0 +1,12 @@
+''' operator precendence
+
+()
+
+**
+
+* /
+
++ -
+
+
+'''
---
+++ 1.python basics 1/4.variable.py
@@ -0,0 +1,9 @@
+x=100 # here in a 100 is stored so a is a variable
+print(x)
+print(id(x))#shows where it stored in our system
+
+#we shouldnt use key word for variables
+a,b,c=1,2,3
+print(a)
+print(b)
+print(c)
